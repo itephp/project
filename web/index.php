b33fc96002675933e7770ed3e0d0cace107a1244
Michał Tomczak <michal@tomczak.net.pl>
2016-05-02 10:58:00 +0200

    Added dir const.

--- web/index.php
+++ web/index.php
@@ -13,12 +13,16 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-require_once __DIR__.'/../vendor/autoload.php';
+define('ITE_ROOT', dirname(__DIR__));
+define('ITE_WEB', ITE_ROOT.'/web');
+define('ITE_SRC', ITE_ROOT.'/src');
+
+require_once ITE_ROOT.'/vendor/autoload.php';
 
 
 $env='prod';
 $debug=false;
-if(file_exists(__DIR__.'/../dev')){
+if(file_exists(ITE_ROOT.'/dev')){
 	$env='dev';
 	$debug=true;
 }
